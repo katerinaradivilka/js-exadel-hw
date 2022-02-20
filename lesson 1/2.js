474f9c8ea723bc861af0c7e1acc21bdc85622d9f
katerinaradivilka <katerinaradivilka@gmail.com>
2022-02-20 23:54:38 +0300

    add solutions for 1st lesson

---
+++ lesson 1/2.js
@@ -0,0 +1,13 @@
+ for (let i=1; i<=100; i++) {
+        if ((i % 3) == 0)
+            console.log("число " + i + " делится на 3");
+        else if ((i % 5) == 0)
+            console.log("число " + i + " делится на 5");
+        else if ((i % 15) == 0)
+            console.log("число " + i + " делится на 3 и 5");
+        else
+            console.log(i);
+}
+ 
+        
+    
---
+++ lesson 1/3.js
@@ -0,0 +1,10 @@
+const prices = [64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 756, 123, 942, 3112, 421, 9341, 1212, 8, 43, 41, 345, 341, 21, 321, 123];
+
+let sum = 0;
+for (let index = 0; index < prices.length; index++) {
+  sum = sum + prices[index];
+}
+let avg = sum / prices.length;
+
+console.log("Avg = " + avg);
+console.log("Sum = " + sum);
