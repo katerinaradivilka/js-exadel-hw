97c8e6f38a4a9c5268985766968e9836706a1580
katerinaradivilka <katerinaradivilka@gmail.com>
2022-02-21 00:19:29 +0300

    add task description for 2 and 3 solutions

--- lesson 1/2.js
+++ lesson 1/2.js
@@ -1,3 +1,11 @@
+ /* Print numbers from 1 to 100 in the console. 
+ If the number is divisible by 3 without a remainder, 
+ then print the “number is divisible by 3” to the console. 
+ If the number is divisible by 5 without a remainder, 
+ then print to the console “the number is divisible by 5”. 
+ If the number is divisible by both 3 and 5 without a remainder, 
+ then you will output to the console “the number is divisible by 3 by 5”.*/
+ 
  for (let i=1; i<=100; i++) {
         if ((i % 3) == 0)
             console.log("число " + i + " делится на 3");
--- lesson 1/3.js
+++ lesson 1/3.js
@@ -1,3 +1,7 @@
+/* There is an array of prices of goods in the receipt. 
+In the console, you need to display the sum of all prices and
+the average price of the product.*/
+
 const prices = [64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 756, 123, 942, 3112, 421, 9341, 1212, 8, 43, 41, 345, 341, 21, 321, 123];
 
 let sum = 0;
